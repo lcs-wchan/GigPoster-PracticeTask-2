134c65ff2be783d6c3b3dcb9a2cbaf3ffdc15147
WilliamChan <wchan@lcs.on.ca>
2020-02-03 11:46:45 -0500

    finished poster

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -38,14 +38,14 @@ let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 canvas.fillColor = yellow
 canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
-// draw grid
-canvas.drawShapesWithBorders = true
-canvas.drawShapesWithFill = false
-for y in stride (from: 0, through: 600, by: 50){
-    for x in stride (from: 0, through: 400, by: 50){
-        canvas.drawRectangle(at: Point(x: x, y: y), width: 50, height: 50)
-    }
-}
+//// draw grid
+//canvas.drawShapesWithBorders = true
+//canvas.drawShapesWithFill = false
+//for y in stride (from: 0, through: 600, by: 50){
+//    for x in stride (from: 0, through: 400, by: 50){
+//        canvas.drawRectangle(at: Point(x: x, y: y), width: 50, height: 50)
+//    }
+//}
 
 // draw brown parallelograms
 canvas.drawShapesWithBorders = false
@@ -88,6 +88,34 @@ for x in stride (from: -130, through: 380, by: 160){
     canvas.drawCustomShape(with: redParallelogram)
     
 }
+
+// draw text
+canvas.drawText(message: "friday, april 13 1984", at: Point(x: 17, y: 420), size: 10)
+canvas.drawText(message: "advance $6.50, door $7.50", at: Point(x: 17, y: 410), size: 10)
+canvas.drawText(message: "no age limit", at: Point(x: 17, y: 400), size: 10)
+
+canvas.drawText(message: "all olympic auditorium", at: Point(x: 150, y: 420), size: 10)
+canvas.drawText(message: "1801 a. grand ave.", at: Point(x: 150, y: 410), size: 10)
+canvas.drawText(message: "los angeles, california", at: Point(x: 150, y: 400), size: 10)
+
+canvas.drawText(message: "with the tourist", at: Point(x: 275, y: 420), size: 10)
+canvas.drawText(message: "muti death cooperations", at: Point(x: 275, y: 410), size: 10)
+canvas.drawText(message: "new regime & red scare", at: Point(x: 275, y: 400), size: 10)
+
+canvas.drawText(message: "subhumans", at: Point(x: 17, y: 330), size: 50)
+
+
+/*:
+ ## Use Source Control
+ 
+ As this is a practice task, there is no hard deadline. However, completing this practice task – an assessment for learning – prior to attempting an assessment of learning – is strongly recommended.
+ 
+ Be sure to commit your work multiple times as you make progress on completing this task.
+ 
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+
 /*:
  ## Use Source Control
  
